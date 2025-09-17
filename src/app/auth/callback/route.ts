3bfab77b237fbb707df5daf639dfbc8cd6447836
Justin Mespel <justin@basd.com>
2025-09-18 08:15:21 +1000

    remove debugger

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -15,7 +15,6 @@ export async function GET(request: Request) {
     if (!error) {
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
-      debugger;
       if (isLocalEnv) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
         return NextResponse.redirect(`${origin}${next}`);
@@ -26,8 +25,6 @@ export async function GET(request: Request) {
       }
     }
   }
-
-  debugger;
 
   // return the user to an error page with instructions
   return NextResponse.redirect(`${origin}/auth/error`);

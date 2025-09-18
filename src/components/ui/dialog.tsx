6641a68606376309208d0d9a5b1abbf62a9e7aee
Justin Mespel <justin@basd.com>
2025-09-18 14:00:15 +1000

    feat: fix touch action on dialog

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -28,6 +28,7 @@ function DialogOverlay({ className, ...props }: React.ComponentProps<typeof Dial
         "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/50",
         className,
       )}
+      style={{ touchAction: "none" }}
       {...props}
     />
   );
@@ -50,6 +51,7 @@ function DialogContent({
           "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg top-[5%] translate-y-0 2xl:top-[50%] 2xl:translate-y-[-50%]",
           className,
         )}
+        style={{ touchAction: "auto" }}
         {...props}
       >
         {children}

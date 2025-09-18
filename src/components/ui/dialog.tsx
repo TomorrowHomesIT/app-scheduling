6ebd73f9c58809389a12b0133e65c4d19b81c7d6
Justin Mespel <justin@basd.com>
2025-09-18 14:21:58 +1000

    fix: revert portal

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -42,7 +42,7 @@ function DialogContent({
   showCloseButton?: boolean;
 }) {
   return (
-    <div>
+    <DialogPortal data-slot="dialog-portal">
       <DialogOverlay />
       <DialogPrimitive.Content
         data-slot="dialog-content"
@@ -63,7 +63,7 @@ function DialogContent({
           </DialogPrimitive.Close>
         )}
       </DialogPrimitive.Content>
-    </div>
+    </DialogPortal>
   );
 }
 

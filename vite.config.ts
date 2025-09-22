43874e71fdc5aaf666bda164d9dfe8c901e56c91
Justin Mespel <justin@basd.com>
2025-09-22 14:15:24 +1000

    fix: try better base URL

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // This ensures absolute paths
   plugins: [
     react(),
     tailwindcss(),
@@ -63,5 +64,4 @@ export default defineConfig({
     chunkSizeWarningLimit: 10000,
     assetsInlineLimit: 0,
   },
-  base: "./", // Relative paths for static hosting
 });

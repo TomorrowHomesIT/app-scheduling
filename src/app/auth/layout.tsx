eedef0968129423f038e57afe5433e1a8eb69261
Justin Mespel <justin@basd.com>
2025-09-26 11:12:39 +1000

    fix: redirect from auth layouts to home page if we end up there after authentication

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,7 +1,22 @@
+import { useAuth } from "@/components/auth/auth-context";
 import { Logo } from "@/components/ui/logo";
-import { Outlet } from "react-router";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router";
 
 export default function AuthLayout() {
+  const { isAuthLoading, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthLoading && isAuthenticated) {
+      navigate("/");
+    }
+  }, [isAuthLoading, isAuthenticated, navigate]);
+
+  if (isAuthLoading || isAuthenticated) {
+    return null;
+  }
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm flex flex-col gap-6">
